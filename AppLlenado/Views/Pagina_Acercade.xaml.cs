3f6c51385a42e4d72b7adc3bad6b21ad954d115e
Umblum12 <abelardo_cruzleos@hotmail.com>
2023-06-08 11:26:40 -0600

    Primer commit app de llenado de agua

---
+++ AppLlenado/Views/Pagina_Acercade.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppLlenado.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Pagina_Acercade : ContentPage
+    {
+        public Pagina_Acercade()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AppLlenado/Views/Pagina_Inicio.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppLlenado.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Pagina_Inicio : ContentPage
+    {
+        public Pagina_Inicio()
+        {
+            InitializeComponent();
+        }
+    }
+}
